2df16b936b04c8b3634d63588037620505d59563
Pavan Gokul <pavan.gokul@thettg.com>
2023-12-04 12:57:48 +0530

    services and Directive

--- TCP01/src/app/modules/pavangokul/pages/home/home.component.ts
+++ TCP01/src/app/modules/pavangokul/pages/home/home.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MemberlistService } from 'src/app/core/services/memberlist.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  
+  constructor(public gokul:MemberlistService){
+    this.Team4List=this.gokul.Team4List
+  }
+    
+  Team4List:string[]=[]
+  ngOnInit(): void {
+  }
 
+  Name="Cristiano Ronaldo"
+  Isfootballer=true
+  getcolor(){
+    return this.Isfootballer?'blue':'red' 
+  }
+
+  
+
+
+  
 }
